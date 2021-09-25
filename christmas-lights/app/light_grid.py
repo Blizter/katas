8bd8fd59985c3d6f83bc5f1541d6e75dfe7d4227
Blizter <blizter47@gmail.com>
2021-09-25 15:37:29 -0400

    adding test cases and implementing turn on method

--- christmas-lights/app/light_grid.py
+++ christmas-lights/app/light_grid.py
@@ -7,8 +7,16 @@ class LightGrid:
     def show_lights(self) -> str:
         return "\n".join(["".join([str(i) for i in row]) for row in self.grid])
 
-    def turn_on(self):
-        pass
+    def turn_on(self, x_1, y_1, x_2, y_2):
+
+        x_2 += 1
+        y_2 += 1
+
+        for i in range(x_1, x_2):
+            for j in range(y_1, y_2):
+                self.grid[i][j] = 1
+
+        return self.grid
 
     def turn_off(self):
         pass
--- christmas-lights/tests/tests.py
+++ christmas-lights/tests/tests.py
@@ -1,1 +1,79 @@
 import pytest
+from app import light_grid
+
+
+turn_on_all_lights_output = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+
+
+turn_on_output_range = [
+    [1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+turn_off_all_lights_output = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+turn_off_range_output = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+
+
+class TestLightGrid:
+
+    grid_height = 10
+    grid_width = 10
+
+    def test_turn_on_all_lights(self):
+
+        test_light_grid = light_grid.LightGrid(height=grid_height, width=grid_width)
+
+        method_output = test_light_grid.turn_on(x_1=0, y_1=0, x_2=9, y_2=9)
+        print(method_output)
+        assert method_output == turn_on_all_lights_output
+
+    def test_turn_on_range(self):
+
+        test_light_grid = light_grid.LightGrid(height=grid_height, width=grid_width)
+
+        method_output = test_light_grid.turn_on(x_1=0, y_1=0, x_2=2, y_2=2)
+        print(method_output)
+        assert method_output == turn_on_output_range
